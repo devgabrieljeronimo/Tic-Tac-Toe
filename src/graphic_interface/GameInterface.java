0027c34bb1b2e6cc39da08553a66f67bfd17d121
Gabriel Jeronimo <gabrieljeronimo2b.007@gmail.com>
2025-06-18 17:28:02 -0300

    Add IconImage

--- src/graphic_interface/GameInterface.java
+++ src/graphic_interface/GameInterface.java
@@ -1,10 +1,26 @@
 package graphic_interface;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GameInterface extends JFrame {
 
     public GameInterface() {
+        setTitle("TicTacToe");
+        ImageIcon gameIcon = new ImageIcon(getClass().getResource("ChatGPTImage06-18-2025,16_51_27.png"));
+        setIconImage(gameIcon.getImage());
+        //setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
+        setBounds(0, 0, 500, 500);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setVisible(true);
+        //setBackground();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void addImage(Image image) {
 
     }
+
+
 }
